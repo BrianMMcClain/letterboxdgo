0eea0ea9fd7239b7808ccc345eca90e54454946f
Brian McClain <brianmmcclain@gmail.com>
2025-11-30 18:07:43 -0500

    Update stats parsing and calculation

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"io"
-	"log"
+	"log/slog"
+	"maps"
 	"os"
+	"slices"
 	"strconv"
 	"time"
 
@@ -13,42 +17,77 @@ import (
 
 func main() {
 
-	file, err := os.Open("./data.csv")
+	//slog.SetLogLoggerLevel(slog.LevelDebug)
+
+	inFilename := flag.String("in", "stats.csv", "Source stats file")
+	flag.Parse()
+
+	slog.Debug("Reading CSV file", "file", *inFilename)
+	inFile, err := os.Open(*inFilename)
 	if err != nil {
-		log.Fatal(err)
+		slog.Error("Could not open stats file", "file", *inFilename, "error", err)
+		os.Exit(-1)
 	}
-	defer file.Close()
+	defer inFile.Close()
 
 	diary := []*letterboxdgo.DiaryEntry{}
 
-	reader := csv.NewReader(file)
+	reader := csv.NewReader(inFile)
+	// Skip headers
+	_, err = reader.Read()
+	if err != nil {
+		slog.Error("Error reading CSV headers", "error", err)
+		os.Exit(-1)
+	}
+
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			slog.Error("Error reading CSV row", "error", err)
+			os.Exit(-1)
 		}
 
 		d := &letterboxdgo.DiaryEntry{}
 		d.Title = record[0]
-		d.Date, _ = time.Parse("2006-01-02", record[1])
-		d.Rating, _ = strconv.Atoi(record[2])
-		d.Liked, _ = strconv.ParseBool(record[3])
-		d.Rewatch, _ = strconv.ParseBool(record[4])
-		d.ReleaseYear, _ = strconv.Atoi(record[5])
+		d.ReleaseYear, _ = strconv.Atoi(record[1])
+		d.Date, _ = time.Parse("2006-01-02", record[2])
+		d.Rating, _ = strconv.Atoi(record[3])
+		d.Liked, _ = strconv.ParseBool(record[4])
+		d.Rewatch, _ = strconv.ParseBool(record[5])
+		d.Slug = record[6]
 
 		diary = append(diary, d)
 	}
 
 	count := 0
+	liked := 0
+	rewatches := 0
+	thisYearCount := 0
+	watches := make(map[string]int)
+
 	for _, entry := range diary {
 		if entry.Date.Year() == 2025 {
-			log.Printf("%+v\n", entry)
 			count++
 		}
+		if entry.Liked {
+			liked++
+		}
+		if entry.Rewatch {
+			rewatches++
+		}
+		if entry.ReleaseYear == 2025 {
+			thisYearCount++
+		}
+
+		watches[entry.Slug]++
 	}
 
-	log.Printf("Total entries in 2025: %d\n", count)
+	fmt.Printf("Total entries in 2025: %d\n", count)
+	fmt.Printf("Unique films: %d\n", len(slices.Collect(maps.Keys(watches))))
+	fmt.Printf("Films liked: %d\n", liked)
+	fmt.Printf("Films rewatched: %d\n", rewatches)
+	fmt.Printf("2025 films watched: %d\n", thisYearCount)
 }
