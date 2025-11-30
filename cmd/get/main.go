4a556f919ca8da4b436f01c706755862c2cec7e4
Brian McClain <brianmmcclain@gmail.com>
2025-11-30 17:32:43 -0500

    Update to use flags and CSV libraries

--- cmd/get/main.go
+++ cmd/get/main.go
@@ -1,20 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"flag"
 	"log/slog"
 	"os"
+	"strconv"
 
 	"github.com/brianmmcclain/letterboxdgo"
 )
 
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
-	username := os.Args[1]
-	d := letterboxdgo.GetDiary(username)
+
+	// Parse flags
+	username := flag.String("username", "", "Letterboxd username")
+	minYear := flag.Int("year", 0, "Pull all diary entries from YEAR to current day")
+	outFilename := flag.String("out", "stats.csv", "File to write stats to")
+	flag.Parse()
+
+	d := letterboxdgo.GetDiary(*username)
+
+	outFile, err := os.Create(*outFilename)
+	if err != nil {
+		slog.Error("Error creating stats.csv", "error", err)
+		os.Exit(-1)
+	}
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	headers := []string{"Title", "ReleaseYear", "Watched", "Rating", "Liked", "Rewatch", "Slug", "TMDB"}
+	err = writer.Write(headers)
+	if err != nil {
+		slog.Error("Error writing headers", "error", err)
+		os.Exit(-1)
+	}
 
 	for _, v := range d {
-		//fmt.Printf("%+v\n", v)
-		fmt.Printf("%s,%s,%d,%t,%t,%d\n", v.Title, v.Date.Format("2006-01-02"), v.Rating, v.Liked, v.Rewatch, v.ReleaseYear)
+
+		if v.Date.Year() < *minYear {
+			continue
+		}
+
+		m := letterboxdgo.GetFilm(v.Slug)
+		row := []string{
+			v.Title,
+			strconv.Itoa(v.ReleaseYear),
+			v.Date.String(),
+			strconv.Itoa(v.Rating),
+			strconv.FormatBool(v.Liked),
+			strconv.FormatBool(v.Rewatch),
+			v.Slug,
+			m.TMDb,
+		}
+		err = writer.Write(row)
+		if err != nil {
+			slog.Error("Error writing row", "slug", v.Slug, "error", err)
+			os.Exit(-1)
+		}
 	}
 }
